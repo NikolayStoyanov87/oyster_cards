1aac3288e318bd44bc0c19ca479839ae204c69b5
Caitlin Cooling <ccooling97@gmail.com>
2018-10-10 14:50:59 +0100

    Fixing git issues

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,13 +1,19 @@
-require "journey"
+require “journey”
 
 describe Journey do
-  let(:station) {double :station, zone: 3}
-  context 'given an entry station' do
-    subject { described_class.new(entry_station: station)}
+ let(:station) {double :station, zone: 3}
+ let(:journey){Journey.new(entry_station, exit_station)}
+ let(:entry_station){“old_street”}
+ let(:exit_station){“old_gate”}
+ context ‘given an entry station’ do
+   subject { described_class.new(entry_station: station)}
 
-    it "stores the entry station" do
-      expect(subject.entry_station).to eq station
-    end
+   it “stores the entry station” do
+     expect(journey.entry_station).to eq entry_station
+   end
 
-  end
-end
+   it “stores the exit station” do
+     expect(journey.exit_station).to eq exit_station
+   end
+ end
+ end
